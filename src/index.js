1de11453029d2c8594bfbac7c352e1e6aee7f76c
Jodar <josedavidangaritar@gmail.com>
2023-11-15 18:39:48 -0500

    otra vaina

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import express from "express"
 import {dirname, join} from "path"
 import { fileURLToPath } from "url"
 
+import indexRoutes from "./routes/index.js"
+
 const app = express()
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.set("views", join(__dirname,"views"))
 app.set("view engine", "ejs")
-
-app.get("/", (req, res) => res.render("index"))
+app.use(indexRoutes)
 
 app.listen(3000)
 console.log("Server is listeing on port", 3000)
