626d330c968b98df769c24336e85adeff27addcc
Jodar <josedavidangaritar@gmail.com>
2023-11-15 20:52:01 -0500

    ahora tiene una perra barra

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 app.set("views", join(__dirname,"views"))
 app.set("view engine", "ejs")
 app.use(indexRoutes)
+app.use(express.static(join(__dirname,"public")))
 
 app.listen(3000)
 console.log("Server is listeing on port", 3000)
